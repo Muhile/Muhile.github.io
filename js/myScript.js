12cac5988f0afa50cad51ea388551f4949c72692
Muhile Hansen <hanscheng94@gmail.com>
2019-01-26 20:56:34 -0600

    Update myScript.js

--- js/myScript.js
+++ js/myScript.js
@@ -1,4 +1,15 @@
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 ctx.fillStyle = "#008080";
-ctx.fillRect(0, 0, 100, 80);
+ctx.fillRect(50, 50, 100, 80);
+
+var drw = canvas.getContext("2d");
+drw.beginPath();
+drw.arc(400, 90, 50, 0, 2 * Math.PI);
+drw.stroke();
+
+var box = canvas.getContext("2d");
+box.font = "20px Comic Sans MS";
+box.fillStyle = "#008080";
+box.textAlign = "center";
+box.fillText("CIT 261 Fluency", canvas.width / 2, 200);
